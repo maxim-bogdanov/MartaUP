bf6915d11984f99b0b2ec59e6dc3b972b21aac75
maxim-bogdanov <937933mb@mail.ru>
2020-03-21 16:32:14 +0300

    работаю над адаптивом

--- js/main.js
+++ js/main.js
@@ -1,21 +1,13 @@
-var mySwiper = new Swiper('.swiper-container', {
-  slidesPerView: 1,
-  loop:true,
-  navigation: {
-    nextEl: '.arrow'
-  },
-  breakpoints: {
-    540: {
-      slidesPerView: 2,
-    }
-  },
+$(function(){
+
+  $('.slider').slick({
+    dots: true,
+    arrows: false,
+    vertical: true,
+    dotsClass: 'header-dots',
+    // swipeToSlide: true,
+    verticalSwiping: true,
+    autoplay: 2000,
+  });
 
 });
-
-var menuButton = document.querySelector('.menu-button-wrap');
-var menuButton1 = document.querySelector('.menu-button');
-var menu = document.querySelector('.header-inner');
-  menuButton.addEventListener('click',function(){
-    menuButton1.classList.toggle('menu-button-active');
-    menu.classList.toggle('header-active');
-  })

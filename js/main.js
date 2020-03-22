94cd5ddc43ced380faac08ead46391063fb68a1c
maxim-bogdanov <937933mb@mail.ru>
2020-03-22 18:52:19 +0300

    почти доделал адаптив

--- js/main.js
+++ js/main.js
@@ -6,8 +6,13 @@ $(function(){
     vertical: true,
     dotsClass: 'header-dots',
     // swipeToSlide: true,
-    verticalSwiping: true,
-    autoplay: 2000
+    // verticalSwiping: true,
+    // autoplay: 2000
+  });
+
+  $('.menu-btn').on('click', function(){
+    $('.menu ul').slideToggle();
+    $('.menu-btn__line').toggleClass('active-btn__line');
   });
 
 });
